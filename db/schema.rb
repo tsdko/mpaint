12d151eb6e4152c452d61ed29b3d088a81992180
flan <2314894+tsdko@users.noreply.github.com>
2026-04-25 22:18:35 +0200

    Generate authentication code

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_04_25_192746) do
+ActiveRecord::Schema[8.1].define(version: 2026_04_25_201608) do
   create_table "images", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.integer "height", default: 400, null: false
@@ -19,4 +19,23 @@ ActiveRecord::Schema[8.1].define(version: 2026_04_25_192746) do
     t.datetime "updated_at", null: false
     t.integer "width", default: 640, null: false
   end
+
+  create_table "sessions", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "ip_address"
+    t.datetime "updated_at", null: false
+    t.string "user_agent"
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_sessions_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "email_address", null: false
+    t.string "password_digest", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email_address"], name: "index_users_on_email_address", unique: true
+  end
+
+  add_foreign_key "sessions", "users"
 end
