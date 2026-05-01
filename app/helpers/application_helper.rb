4835420cc25f75ccfc803a4eb113f229a1f210c9
flan <2314894+tsdko@users.noreply.github.com>
2026-05-01 20:20:36 +0200

    Use helper instead of partial for standard <time>

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,20 @@
 module ApplicationHelper
+  def time_tag(datetime, relative: false, data: {}, **options)
+    # time tags with datetime present are automatically localized to the user's
+    # timezone via js Util.localizeDateTimes; data-is-relative determines whether
+    # to replace the title or the content
+
+    main = datetime
+    sub = time_ago_in_words(datetime) + " ago"
+    main, sub = sub, main if relative
+
+    data[:is_relative] = true if relative
+
+    tag.time(datetime: datetime.rfc3339, data: data, title: sub, **options) do
+      main
+    end
+  end
+
   def svg_icon(name, class: nil, **options)
     cl = binding.local_variable_get :class
     tag.svg(class: "inline-block h-lh w-[1lh] #{cl}".strip, **options) do
