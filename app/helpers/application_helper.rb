012838dcc6edc86d07f880c32345c0501cb7b56d
flan <2314894+tsdko@users.noreply.github.com>
2026-05-01 20:07:28 +0200

    Add some vector icons

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def svg_icon(name, class: nil, **options)
+    cl = binding.local_variable_get :class
+    tag.svg(class: "inline-block h-lh w-[1lh] #{cl}".strip, **options) do
+      tag.use(fill: "currentColor", href: asset_path("icons.svg") + "##{name}")
+    end
+  end
 end
