db5a28863c09042812bc9702bee360ef3d4534c3
flan <2314894+tsdko@users.noreply.github.com>
2026-05-07 21:27:56 +0200

    Add size to stroke test data; instantiate commands via constructor

--- test/models/image/stroke_test.rb
+++ test/models/image/stroke_test.rb
@@ -7,15 +7,19 @@ class Image::StrokeTest < ActiveSupport::TestCase
 
   test "should convert from wire format to DB format" do
     s = Image::Stroke.new(image_id: @stroke.image_id)
-    s.push_cmd(CanvasCommand::from_h({"t" => "line", "pointer_id" => 0, "p1" => {"x" => 1, "y" => 2}, "p2" => {"x" => 3, "y" => 4}}))
-    s.push_cmd(CanvasCommand::from_h({"t" => "line", "pointer_id" => 0, "p1" => {"x" => 3, "y" => 4}, "p2" => {"x" => 6, "y" => 9}}))
-    s.add_brush_delta({color: CanvasCommand::from_h({"t" => "color", "r" => 255, "g" => 0, "b" => 255})})
+    s.push_cmd(CanvasCommand::Line.new(pointer_id: 0, p1: {"x" => 1, "y" => 2}, p2: {"x" => 3, "y" => 4}))
+    s.push_cmd(CanvasCommand::Line.new(pointer_id: 0, p1: {"x" => 3, "y" => 4}, p2: {"x" => 6, "y" => 9}))
+    s.add_brush_delta({
+      color: CanvasCommand::Color.new(r: 255, g: 0, b: 255),
+      size: CanvasCommand::Size.new(size: 10),
+    })
     assert_equal @stroke.data, s.data
   end
 
   test "should convert from DB format to wire format" do
     wire = [
       {t: "color", r: 255, g: 0, b: 255},
+      {t: "size", size: 10},
       {t: "line", p1: {x: 1, y: 2}, p2: {x: 3, y: 4}},
       {t: "line", p1: {x: 3, y: 4}, p2: {x: 6, y: 9}},
     ].map { |d| d.update({pid: @stroke.participation_id}) }
