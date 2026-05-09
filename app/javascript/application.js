162d1bac3435e6f48daf37b79aaee196ebedae9b
flan <2314894+tsdko@users.noreply.github.com>
2026-05-09 04:45:12 +0200

    Add Stimulus

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 
+import "controllers"
 import * as Util from "util";
 import * as ImageCanvas from "image_canvas";
 
