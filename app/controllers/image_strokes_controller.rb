5173ec679365296da610417acaee35e97041cd6a
flan <2314894+tsdko@users.noreply.github.com>
2026-05-10 20:14:37 +0200

    Fix N+1 in image_strokes#index

--- app/controllers/image_strokes_controller.rb
+++ app/controllers/image_strokes_controller.rb
@@ -2,7 +2,7 @@ class ImageStrokesController < ApplicationController
   allow_unauthenticated_access
 
   def index
-    @strokes = Image::Stroke.where(image_id: params[:image_id])
+    @strokes = Image::Stroke.where(image_id: params[:image_id]).includes(:participation)
     respond_to do |format|
       format.html
       format.any(:xml, :json) { render request.format.to_sym => @strokes.map { |s| s.wire_data } }
