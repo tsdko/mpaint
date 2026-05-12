f2c22d1cd355c60d116f614ad945703756646394
flan <2314894+tsdko@users.noreply.github.com>
2026-05-12 02:15:31 +0200

    Use Turbo frames for message-related UI

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,9 +1,14 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 
-import "controllers"
+import "@hotwired/turbo-rails";
+Turbo.session.drive = false;
+import "controllers";
 import * as Util from "util";
 import * as ImageCanvas from "image_canvas";
 
-Util.localizeDateTimes();
+[
+  "DOMContentLoaded",
+  "turbo:frame-load",
+].forEach(event => document.addEventListener(event, () => Util.localizeDateTimes()));
 window.Util = Util;
 window.ImageCanvas = ImageCanvas;
--- config/importmap.rb
+++ config/importmap.rb
@@ -10,3 +10,4 @@ pin "@rails/actioncable", to: "@rails--actioncable.js" # @8.1.300
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin "@hotwired/turbo-rails", to: "turbo.min.js"
