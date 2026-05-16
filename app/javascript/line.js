a958227baf06c06e047a565819979f68a73c10ca
flan <2314894+tsdko@users.noreply.github.com>
2026-05-16 22:49:53 +0200

    Implement thick sharp lines; lay groundwork for custom brushes

--- app/javascript/line.js
+++ app/javascript/line.js
@@ -1,18 +1,46 @@
 // Utilities for drawing lines on html5 canvas.
 
-function set_pixel(d, x, y, c) {
-  let idx = (y*d.width + x)*4;
-  d.data[idx] = c.r;
-  d.data[idx+1] = c.g;
-  d.data[idx+2] = c.b;
-  d.data[idx+3] = 255;
+function brush_canvas(color, thickness) {
+  const bcs = thickness * 2;
+  const bc = new OffscreenCanvas(bcs, bcs);
+  const bctx = bc.getContext("2d");
+  bctx.fillStyle = bctx.strokeStyle = `rgb(${color.r}, ${color.g}, ${color.b})`;
+  bctx.filter = Util.noAntialiasFilter;
+  bctx.ellipse(bc.width/2, bc.height/2, thickness/2, thickness/2, 0, 0, Math.PI*2);
+  bctx.stroke();
+  bctx.fill();
+  return bc;
+};
+
+function draw_funcs(canvas, color, thickness) {
+  const ctx = canvas.getContext("2d");
+  if(thickness > 1) {
+    const bc = brush_canvas(color, thickness);
+    return {
+      draw: (x, y, c) => ctx.drawImage(bc, x - thickness/2, y - thickness/2),
+      finish: () => {},
+    };
+  } else {
+    const d = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    return {
+      draw: (x, y, c) => {
+        let idx = (y*d.width + x)*4;
+        d.data[idx] = c.r;
+        d.data[idx+1] = c.g;
+        d.data[idx+2] = c.b;
+        d.data[idx+3] = 255;
+      },
+      finish: () => ctx.putImageData(d, 0, 0),
+    };
+  }
 }
 
-function line_bresenham(x0, y0, x1, y1, c) {
+function line_bresenham(x0, y0, x1, y1, c, thickness) {
   let dx = Math.abs(x1 - x0),
       dy = Math.abs(y1 - y0),
       flipped = false;
-  const data = new ImageData(dx+1, dy+1);
+  const canvas = new OffscreenCanvas(dx+thickness, dy+thickness);
+  const {draw, finish} = draw_funcs(canvas, c, thickness);
 
   if(dx < dy) {
     [dx, dy] = [dy, dx];
@@ -27,9 +55,9 @@ function line_bresenham(x0, y0, x1, y1, c) {
 
   for(let d = d_init, y = y0, x = x0; x != x1; x0 < x1 ? ++x : --x) {
     if(flipped)
-      set_pixel(data, y, x, c);
+      draw(y, x, c);
     else
-      set_pixel(data, x, y, c);
+      draw(x, y, c);
 
     if(d < 0) {
       d += d_eq;
@@ -39,7 +67,8 @@ function line_bresenham(x0, y0, x1, y1, c) {
     }
   }
 
-  return window.createImageBitmap(data);
+  finish();
+  return new Promise(resolve => resolve(canvas));
 }
 
 export function sharp(ctx, p1, p2, brush) {
@@ -61,12 +90,13 @@ export function sharp(ctx, p1, p2, brush) {
   p2.x -= xm;
   p1.y -= ym;
   p2.y -= ym;
+  const size = brush.size ?? 1;
+  const so = (size - 1)/2;
 
   // implementation reminder: whatever method you use, make sure
   // it respects ctx.globalCompositeOperation
-  // TODO: support thicker lines
-  line_bresenham(p1.x, p1.y, p2.x, p2.y, brush?.color ?? {r: 0, g: 0, b: 0})
-    .then(bitmap => ctx.drawImage(bitmap, xm, ym));
+  line_bresenham(p1.x, p1.y, p2.x, p2.y, brush?.color ?? {r: 0, g: 0, b: 0}, brush.size ?? 1)
+    .then(bitmap => ctx.drawImage(bitmap, xm - so, ym - so));
 }
 
 export function regular(ctx, p1, p2, brush) {
